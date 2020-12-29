94794086bf09ef51aed02b3f2e205440f40b290b
Hannah Catherine Trask <hctrask@gmail.com>
2020-12-29 12:58:38 -0700

    placeholders fetched

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,11 +1,36 @@
 import BaseLayout from '../components/layouts/baselayout';
+import React from 'react';
+import axios from 'axios';
 
-const Gallery = () => {
-	return (
-		<BaseLayout>
-			<h1>this is the Gallery page</h1>
-		</BaseLayout>
-	);
-};
+class Gallery extends React.Component {
+	static async getInitialProps() {
+		let photos = [];
+		try {
+			const res = await axios.get(
+				'https://jsonplaceholder.typicode.com/photos'
+			);
+			photos = await res.data;
+		} catch (e) {
+			console.error(e);
+		}
+
+		return { photos: photos.slice(0, 10) };
+	}
+
+	renderPhotos(photos) {
+		return photos.map((photo) => <li key={photo.id}>{photo.id}</li>);
+	}
+
+	render() {
+		const { photos } = this.props;
+		console.log(photos);
+		return (
+			<BaseLayout>
+				<h1>this is the Gallery page</h1>
+				<ul>{this.renderPhotos(photos)}</ul>
+			</BaseLayout>
+		);
+	}
+}
 
 export default Gallery;
